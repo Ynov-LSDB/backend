7886fd181d7ead040797f05ea6cf1ce7c4c3d69a
QuentinSAIL <saillardq@gmail.com>
2023-11-23 12:31:37 +0100

    :zap: improve seeder

--- database/factories/DrinkFactory.php
+++ database/factories/DrinkFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Drink;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,15 +17,26 @@ class DrinkFactory extends Factory
      */
     public function definition(): array
     {
-        $degree = fake()->numberBetween(0, 100);
+        $degree = fake()->numberBetween(0, 80);
 
         return [
-            'title' => fake()->name(),
+            'title' => "",
             'description' => fake()->text(),
             'degree' => $degree,
             'imageURL' => fake()->imageUrl(640, 480, 'drink'),
             'is_cuite_possible' => $degree < 5 ? false : true,
             'nbr_ice_max' => fake()->numberBetween(0, 3),
         ];
+    }
+
+    public function configure(): self
+    {
+        return $this->afterCreating(function (Drink $drink) {
+            $boissons = ["Ricard", "Vodka", "Whisky", "Rhum", "Tequila", "Gin", "Vermouth", "Cognac", "Champagne", "Martini", "Bourbon", "Sambuca", "Baileys", "Cointreau", "Pisco", "Absinthe", "Amaretto", "Saké", "Prosecco", "Brandy", "Campari", "Jägermeister", "Kalhua", "Grand Marnier", "Chambord", "Chartreuse", "Irish Cream", "Midori", "Drambuie", "Armagnac", "Limoncello", "Pimm's", "Pastis", "Grappa", "Crème de Menthe", "Soju", "Pina Colada", "Mojito", "Margarita", "White Russian", "Dark and Stormy", "Bellini", "Moscow Mule", "Negroni", "Piña Colada", "Sex on the Beach", "Mai Tai", "Mojito", "Caipirinha", "Old Fashioned"];
+            $descriptions = ["Comme un feu de camp dans une bouteille - réchauffe l'âme et les orteils en même temps.", "C'est comme une fête dans un verre, avec la menthe qui fait le moonwalk sur votre langue.", "L'eau de vie qui transforme instantanément n'importe quelle soirée en fête.", "La potion magique qui transforme les timides en danseurs de salsa impromptus.", "Comme une forêt enchantée dans un verre - chaque gorgée est une balade dans les bois.", "Capture l'esprit des îles en une gorgée - le soleil, la mer et une brise légère.", "L'élégance en verre, secoué avec une touche de sophistication.", "Les bulles sont comme des éclats de rire dans une célébration perpétuelle.", "Un peu comme une soirée en forêt avec des amis - épicé, mémorable et un peu mystérieux.", "Un voyage instantané vers une plage tropicale - la gourmandise et la détente dans chaque gorgée."];
+            $drink->title = $boissons[$drink->id - 1];
+            $drink->description = $descriptions[rand(0, count($descriptions) - 1)];
+            $drink->save();
+        });
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
         Category::Factory()->count(10)->create();
         Rank::Factory()->count(5)->create();
         Role::Factory()->count(2)->create();
-        Drink::Factory()->count(5)->create();
+        Drink::Factory()->count(50)->create();
         User::Factory()->count(20)->create();
         Event::Factory()->count(10)->create();
         DrinkEvent::Factory()->count(10)->create();
